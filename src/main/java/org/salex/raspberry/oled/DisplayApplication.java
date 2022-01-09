a671f4ecd4e6ca1a8fb4858490a354c05f53dbd0
丁文选 <wenxuan.ding@pintec.com>
2022-01-09 20:12:58 +0800

    ip

--- src/main/java/org/salex/raspberry/oled/DisplayApplication.java
+++ src/main/java/org/salex/raspberry/oled/DisplayApplication.java
@@ -8,6 +8,10 @@ import com.pi4j.wiringpi.I2C;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class DisplayApplication {
@@ -22,10 +26,47 @@ public class DisplayApplication {
             display = new Display(128, 64, gpio, i2c, 0x3c);
             display.begin();
             display.clear();
-//            display.setPixel(10,10, true);
-            display.displayString("中abcd国");
+            display.displayString("IP:" + getLocalHostLANAddress().getHostAddress());
         } catch (I2CFactory.UnsupportedBusNumberException | IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    // 正确的IP拿法，即优先拿site-local地址
+    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException(
+                    "Failed to determine LAN address: " + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
         }
     }
 }
